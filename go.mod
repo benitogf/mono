466927e544b7eec3ec7d467602233885b09be8c6
root <root@DESKTOP-5PMS49A>
2023-06-06 14:36:05 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/benitogf/mono
+
+go 1.19
+
+require github.com/webview/webview v0.0.0-20230602035950-899018ad0e5c
--- src/App.js
+++ src/App.js
@@ -2,24 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/* had to add this to change the default absolute path */}
+                <img src={'.' + logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <p>
+                    logo? {JSON.stringify(logo)}
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
